64244074fe008a108613113515c9c18ec1043c29
Paul Rayner <paul@virtual-genius.com>
2015-01-06 11:04:09 -0700

    Tidy up build script a little

--- build.sh
+++ build.sh
@@ -16,21 +16,18 @@ set -e
 echo -e "Building jekyll site (to _site by default)\n"
 jekyll build
 
-echo
-rm -rf ../paulrayner.github.com.master
-
-# Clone `master' branch of the repository using encrypted GH_TOKEN for authentification
+# Use encrypted GH_TOKEN for authentification
 echo -e "Cloning master branch of Jekyll site\n"
 git clone https://${GH_TOKEN}@github.com/paulrayner/paulrayner.github.com.git ../paulrayner.github.com.master
-
 
 echo -e "Copy generated HTML site to master branch"
 cp -R _site/* ../paulrayner.github.com.master
 
-# commit and push generated content to `master' branch
-# since repository was cloned in write mode with token auth - we can push there
-echo -e "Committing site\n"
+echo -e "Committing and pushing site to master branch on GitHub pages repository\n"
 cd ../paulrayner.github.com.master
 git add -A .
 git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
 git push --quiet origin master > /dev/null 2>&1
+
+echo -e "Cleaning up"
+rm -rf ../paulrayner.github.com.master
