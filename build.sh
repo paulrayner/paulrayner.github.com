935231aba0db13e41c5524c0d0835bb2506cf5c0
Paul Rayner <paul@virtual-genius.com>
2015-01-13 12:09:06 -0700

    Updates to build script to support automatic draft publishing.

--- build.sh
+++ build.sh
@@ -9,6 +9,10 @@ fi
 # Enable error reporting to the console
 set -e
 
+# Auto-publish the next draft
+echo -e "Auto-publishing the next draft\n"
+rake publish
+
 echo -e "Building jekyll site (to _site by default)\n"
 jekyll build
 
@@ -25,8 +29,8 @@ git config user.email "paul@virtual-genius.com"
 git config user.name "Paul Rayner"
 git add -A .
 git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
-git push --quiet --force origin master > /dev/null 2>&1
+#git push --quiet --force origin master > /dev/null 2>&1
 
 echo -e "Cleaning up"
-rm -rf ../paulrayner.github.com.master
+#rm -rf ../paulrayner.github.com.master
 rm -rf _site
